18a18bf832d5bc5f80ee48c94af1516493666de1
EasyLife <EasyLife@M-T-G>
2024-05-06 18:01:17 +0300

    Add project files.

---
+++ task_1/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task_1.Data;
+
+namespace task_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext Context=new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var employees=Context.employees.ToList();
+            return View("Index", employees);
+        }
+    }
+}
---
+++ task_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task_1.Models;
+
+namespace task_1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;Database=ASP81;trusted_connection=true;TrustServerCertificate=true;");
+        }
+    }
+}
